083a547a9e0808f3014d0b66e2244d8560ca5446
Asiwaju-Adeniyi <makindeadeniyi1@gmail.com>
2026-01-28 01:23:22 +0100

    Update elemWiseMax.py

--- elemWiseMax.py
+++ elemWiseMax.py
@@ -1,14 +1,1 @@
-def naive_relu(x):
 
-  #x is a rank-2 NumPy tensor.
-
-  assert len(x.shape)==2
-
-  #Avoids overwriting the input tensor
-  x = x.copy()
-
-  for i in range(x.shape[0]):
-    for j in range(x.shape[1]):
-      x[i,j] = max(x[i,j],0)
-
-  return x
