e44144d441502e6f2e60e9d6371d7a17126de5f5
Asiwaju-Adeniyi <makindeadeniyi1@gmail.com>
2026-01-28 01:23:36 +0100

    Correct naive_add function implementation
    Fix indentation and return statement in naive_add function.

--- elemWiseNaiveAdd.py
+++ elemWiseNaiveAdd.py
@@ -1,14 +1,1 @@
-def naive_add(x,y): 
-  #x and y are rank-2 Numpy tensors.
 
-  assert len(x.shape) == 2
-  assert x.shape == y.shape
-  #Avoids overwriting the input tensor
-
-  x = x.copy() 
-
-  for i in range(x.shape[0]):
-    for j in range(x.shape[1]):
-      x[i,j] += y[i,j]
-
-      return x
