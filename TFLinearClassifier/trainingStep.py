c717921e4c28a8173786c50d4f088070a3d4df3c
Asiwaju-Adeniyi <makindeadeniyi1@gmail.com>
2026-01-28 15:56:50 +0100

    Remove unnecessary comment from trainingStep.py
    Remove commented-out training step line.

---
+++ TFLinearClassifier/trainingStep.py
@@ -0,0 +1,16 @@
+learning_rate = 0.1
+
+#Using XLA compilation 
+@tf.function(jit_compile=True)
+def training_step(inputs, targets, W, b):
+    # Forward pass, inside of a gradient tape scope
+    with tf.GradientTape() as tape:
+        predictions = model(inputs, W, b)
+        loss = mean_squared_error(predictions, targets)
+    # Retrieves the gradient of the loss with regard to weights
+    grad_loss_wrt_W, grad_loss_wrt_b = tape.gradient(loss, [W, b])
+    # Updates the weights
+    W.assign_sub(grad_loss_wrt_W * learning_rate)
+    b.assign_sub(grad_loss_wrt_b * learning_rate)
+    return loss
+
