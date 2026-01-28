faf3ec32c440a127eb0c6c42e2b51b7539665754
Asiwaju-Adeniyi <makindeadeniyi1@gmail.com>
2026-01-28 15:53:52 +0100

    Create sepData.py

---
+++ TFLinearClassifier/sepData.py
@@ -0,0 +1,13 @@
+import numpy as np
+import tensorflow as tf
+
+num_samples_per_class = 1000
+negative_samples = np.random.multivariate_normal(
+    mean = [3,0], cov = [[1, 0.5], [0.5, 1]], size = num_samples_per_class
+)
+
+positive_samples = np.random.multivariate_normal(
+    mean = [0,3], cov = [[1, 0.5], [0.5, 1]], size = num_samples_per_class
+)
+
+inputs = np.vstack((negative_samples, positive_samples)).astype(np.float32)
